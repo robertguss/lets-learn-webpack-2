c18d80fbefc4e710294f3a051e557b8985ff515a
Robert Guss <robertguss@gmail.com>
2017-08-25 14:19:14 -0400

    added scss source maps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './css/main.css';
-// import './scss/main.scss';
+// import './css/main.css';
+import './scss/main.scss';
 import {hello, sup} from './js/module';
 
 hello();
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,11 @@ module.exports = {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
+          use: [
+            {loader: 'css-loader', options: {sourceMap: true} },
+            {loader: 'postcss-loader', options: {sourceMap: true} },
+            {loader: 'sass-loader', options: {sourceMap: true} }
+          ]
         })
       },
       {
